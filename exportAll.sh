56b7a834aac1b621d2b2e17c21b32ba930e68a05
znibb <znibb@zkylark.se>
2020-07-30 01:01:04 +0200

    Added wrapper script for exporting new json files

---
+++ exportAll.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+python exportAlchemy.py
+python exportBlacksmithing.py
+python exportCooking.py
+python exportEnchanting.py
+python exportEngineering.py
+python exportItemID.py
+python exportLeatherworking.py
+python exportTailoring.py
+python exportVendorReagents.py
